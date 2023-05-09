bd3b13007100ebe75d7f334e10baf206c61a0858
Diegoc24 <carusodiegod@gmail.com>
2023-05-09 14:28:12 -0300

    feacture/courseModel(Backend/Models): Created Course Model

---
+++ database/models/Course.model.js
@@ -0,0 +1,38 @@
+const sequelize = require("./index.js")
+const {Sequelize, DataTypes} = require("sequelize");
+
+const course = sequelize.define( "course", 
+{
+    
+        id: {
+          type: DataTypes.UUID,
+          defaultValue: Sequelize.UUIDV4,
+          primaryKey: true,
+          allowNull: false,
+        },
+
+        title: {
+            type: DataTypes.STRING(50),
+            allowNull: false,
+            
+        },
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        background_image: {
+            type: DataTypes.STRING(50),
+            allowNull: false,
+           
+        },
+        released_date: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW
+        },
+    },
+        {
+            timestamps: false
+        }
+);
+
+module.exports = course;
