a8b9bd9543cd851c4cb68bc03488a685c85a2733
Fer-Mig-Agus <miguelfernandez.m.f.8998@gmail.com>
2023-05-14 16:14:12 -0300

    feature/routesCourse (backend/controllers): I have modified the get controllodor of coursComment

--- server/settings/controllers/commentCourse/getAllCommentcourseById.controller.js
+++ server/settings/controllers/commentCourse/getAllCommentcourseById.controller.js
@@ -1,12 +1,18 @@
+
 const Course = require("../../../../database/models/course.model");
 const CourseComment = require("../../../../database/models/courseComment.model")
+const User = require("../../../../database/models/user.model");
+
 
 const getAllCommentcourseById = async (req, res) => {
 
     const { idCourse } = req.query;
+    console.log(idCourse);
 
     try {
+
         const course = await Course.findByPk(idCourse);
+
         if (!course) return res.status(400).json({ error: "Course not found" });
 
         const allCommentCourse = await CourseComment.findAll({
@@ -15,7 +21,38 @@ const getAllCommentcourseById = async (req, res) => {
             }
         });
 
-        res.status(200).json(allCommentCourse);
+        if (allCommentCourse.length === 0) return res.status(400).json({ error: "No hay comentarios de ese curso" });
+
+
+        const actuallyInfomation = async (array) => {
+
+            const newArray = [];
+
+            for (let i = 0; i < array.length; i++) {
+
+                const userId = array[i].userId;
+                const userById = await User.findByPk(userId);
+
+                const newInformation = {
+                    id: array[i].id,
+                    message: array[i].message,
+                    createdAt: array[i].createdAt,
+                    updatedAt: array[i].updatedAt,
+                    user: userById,
+                    course: course,
+
+                }
+
+                newArray.push(newInformation);
+            }
+
+            return newArray;
+        }
+
+        const allCommentData = await actuallyInfomation(allCommentCourse);
+
+
+        res.status(200).json(allCommentData);
 
     } catch (error) {
 
