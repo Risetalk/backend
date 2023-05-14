d5514c6ba9e0b01e6f380e48e3cfc08f5e01a1ff
Fer-Mig-Agus <miguelfernandez.m.f.8998@gmail.com>
2023-05-13 22:51:50 -0300

    feature/routesCourse (backend/controllers):  I have modified Course's get controller by id

--- server/settings/controllers/course/courseById.controller.js
+++ server/settings/controllers/course/courseById.controller.js
@@ -1,11 +1,34 @@
 const Course  = require("../../../../database/models/course.model")
+const Video = require("../../../../database/models/video.model");
 
 const courseById =  async (req,res)=>{
     const {id} = req.params;
     try {
 
         const courseById = await Course.findOne({where: {id}})
-        res.status(200).send(courseById)
+        console.log(courseById.id);
+
+
+        const allVideoofCourse = await Video.findAll({
+            where: {
+                courseId: courseById.id
+            }
+        });
+
+        const allInformationCourse={
+            id: courseById.id,
+            title: courseById.title,
+            description: courseById.description,
+            background_image: courseById.background_image,
+            released_date: courseById.released_date,
+            rating: courseById.rating,
+            price: courseById.price,
+            video: allVideoofCourse.length,
+            createdAt: courseById.createdAt,
+            updatedAt: courseById.updatedAt,
+        }
+ 
+        res.status(200).send(allInformationCourse)
         
     } catch (error) {
 
