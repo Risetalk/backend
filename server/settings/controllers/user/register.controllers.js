3cba83c7afa0522c2d3eabe9dd880bca536e74ea
Diego Ibañez <diegoibanez336@gmail.com>
2023-05-23 13:44:20 -0500

    feacture/controllerRegister(backend/controller): validation is performed that does not enter repeated usernames

--- server/settings/controllers/user/register.controllers.js
+++ server/settings/controllers/user/register.controllers.js
@@ -8,43 +8,43 @@ dotenv.config();
 // Endpoint for user registration
 const registroUser = async (req, res) => {
     try {
-        // We take the data that comes by body
-        const { first_name, last_name, user_name, email, password, date_birth } = req.body;
-        
-        // We look for the user by email 
-        const existeUsuario = await User.findOne({ where: { email } })
-        // We verify that you are registered
-        if (existeUsuario) {
-            const error = new Error ("User already exists")
-             return res.status(404).json({message:error.message})
-        }
-        // We encrypt the password
-        const salt = await bcrypt.genSalt();
-        const hashedPassword = await bcrypt.hash(password, salt);
-        // We create an instance of the user by passing the encrypted password
-        const user = new User({
-            first_name,
-            last_name,
-            user_name,
-            email,
-            password: hashedPassword,
-            date_birth,
-            is_tutor: true
-
-        });
-        
-        // We generate the user confirmation token
-        user.token = generarIdToken()
-        // We save the user in the database
-        await user.save();
-        // We send the token to the user's email
-       await emailRegistro(user)
-        
-        res.status(200).send({ message: 'User created successfully' })
+      // Get the data that comes in the request body
+      const { first_name, last_name, user_name, email, password, date_birth } = req.body
+  
+     // Check if a user with the same username already exists
+      const existeUsuario = await User.findOne({ where: { user_name } })
+      if (existeUsuario) {
+        const error = new Error("Username already exists")
+        return res.status(400).json({ error: error.message })
+      }
+  
+      // We check if a user already exists with the same email
+      const existeEmail = await User.findOne({ where: { email } })
+      if (existeEmail) {
+        const error = new Error("Email already exists")
+        return res.status(400).json({ error: error.message })
+      }
+      const salt = await bcrypt.genSalt();
+      const hashedPassword = await bcrypt.hash(password, salt);
+      const user = new User({
+        first_name,
+        last_name,
+        user_name,
+        email,
+        password: hashedPassword,
+        date_birth,
+        is_tutor: true
+      });
+      user.token = generarIdToken()
+      await user.save();
+      await emailRegistro(user)
+  
+      res.status(200).send({ message: 'User created successfully' })
     } catch (error) {
-        res.status(400).json({ error: error.message })
+      res.status(400).json({ error: error.message })
     }
-}
+  }
+  
 
 
 module.exports = registroUser;
