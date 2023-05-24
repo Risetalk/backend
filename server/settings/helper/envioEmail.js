28038d6a9f186f33aaa386be7a802b1db6374781
Diegoc24 <carusodiegod@gmail.com>
2023-05-23 23:06:14 -0300

    feature/apoyoUser modification in the sending of e-mails

--- server/settings/helper/envioEmail.js
+++ server/settings/helper/envioEmail.js
@@ -1,6 +1,14 @@
 const nodemailer = require('nodemailer')
 require("dotenv").config()
+const path = require('path');
+const fs = require('fs');
 
+// Obtén la ruta absoluta al archivo HTML
+const htmlFilePath = path.resolve(__dirname, 'ValidateEmail.html');
+const cssFilePath = path.resolve(__dirname, 'style.css');
+// Lee el contenido del archivo HTML
+const htmlContent = fs.readFileSync(htmlFilePath, 'utf8');
+const cssContent = fs.readFileSync(cssFilePath, "utf-8")
 // utilizamos nodemailer para hacer el envio de email funcion para confirmar el usuario
 const emailRegistro = async(user) => {
     const transport = nodemailer.createTransport({
@@ -12,31 +20,17 @@ const emailRegistro = async(user) => {
             pass: process.env.CONTRASENIA_GMAIL_APLICACION // generated ethereal password
         }
     })
-    const email_confirmation_html = `
-<div style="background-color: #F4F4F4; text-align: center; padding: 20px;">
-  <img src="https://i.ibb.co/dQLRTZv/risetalk-Logo.png" alt="RiseTalk Logo" style="height: 100px; margin: 0 auto;">
-  <div style="background-color: #FFFFFF; width: 80%; margin: 20px auto; text-align: center; padding: 10px 20px;">
-    <p style="font-size: 24px;">
-    Hello ${user.user.dataValues.first_name}, validate your account in RiseTalk
-    </p>
-    <p>Your account is almost ready, you just have to check it in the following link:</p>
-    <div style="display: inline-block;">
-      <a href="${process.env.FRONTEND_URL}/user/confirmar/${user.user.dataValues.token}" style="color: black; text-decoration: none;">
-        <span style="display: inline-block; margin: 10px auto; background-color: #F0713D; border: 2px solid #F35210; padding: 8px; border-radius: 5px;">
-          <span style="display: inline-block; cursor: pointer;">Confirm Account</span>
-        </span>
-      </a>
-    </div>
-    <p>If you did not request the validation of your account, you can ignore the message</p>
-  </div>
-</div>
-`;
+    
+const htmladdcss = htmlContent.replace(/<style>[\s\S]*<\/style>/, `<style>${cssContent}</style>`)
+
+console.log(htmladdcss);
+
       await transport.sendMail({
         from:'"RiseTalk - Account Manager" <accounts@RiseTalk.com>',
         to: user.user.dataValues.email,
         subject: 'Confirm Your Account',
         text: 'Check your account on RiseTalk',
-        html: email_confirmation_html
+        html: htmladdcss
       })
 }
 
@@ -51,24 +45,7 @@ const olvidePassword = async(user) => {
       pass: process.env.CONTRASENIA_GMAIL_APLICACION,
     }
     });
-    const password_reset_html = `
-<div style="background-color: #F4F4F4; text-align: center; padding: 20px;">
-  <img src="https://i.ibb.co/dQLRTZv/risetalk-Logo.png" alt="RiseTalk Logo" style="height: 100px; margin: 0 auto;">
-  <div style="background-color: #FFFFFF; width: 80%; margin: 20px auto; text-align: center; padding: 10px 20px;">
-    <p style="font-size: 24px;">Hello ${user.user.dataValues.first_name} you have requested to reset your RiseTalk password</p>
-    <p>Follow the link below to generate a new password:</p>
-    <div style="display: inline-block;">
-      <a href="${process.env.FRONTEND_URL}/login/forgetpassword/${user.user.dataValues.token}" style="color: black; text-decoration: none;">
-        <span style="display: inline-block; margin: 10px auto; background-color: #F0713D; border: 2px solid #F35210; padding: 8px; border-radius: 5px;">
-          <span style="display: inline-block; cursor: pointer;">Reset Password</span>
-        </span>
-      </a>
-    </div>
-    <p>If you did not request to reset the password, you can ignore the message</p>
-  </div>
-</div>
-`;
-
+    
     await transport.sendMail({
       from:'"RiseTalk - Administrador de cuentas" <cuentas@RiseTalk.com>',
       to: user.user.dataValues.email,
