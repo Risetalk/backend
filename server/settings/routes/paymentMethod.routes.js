165064c302ebf5dcc2a0aec3ada8bee6219e3055
JesseMcCallums <jessemccallums2001@gmail.com>
2023-05-30 19:11:34 -0400

    Update back

--- server/settings/routes/paymentMethod.routes.js
+++ server/settings/routes/paymentMethod.routes.js
@@ -2,15 +2,18 @@
 const { Router } = require("express");
 
 // Local Dependencies.
-const postFinalPayment = require("../controllers/paymentMethod/postFinalPayment.controller");
+//const postFinalPayment = require("../controllers/paymentMethod/postFinalPayment.controller");
+const paymentFinal = require("../controllers/paymentMethod/postFinalPayement.controller");
+
+//const configPayment = require("../controllers/paymentMethod/configPayment.controller");
 
 const configPayment = require("../controllers/paymentMethod/configPayment.controller");
 
 // Router Instance.
-//const paymentMethodRoutes = Router();
+const paymentMethodRoutes = Router();
 
 //POST method to pay for a course
-paymentMethodRoutes.post("/create-payment", postFinalPayment);
+paymentMethodRoutes.post("/create-payment", paymentFinal);
 
 // Get payment methods by id.
 paymentMethodRoutes.get("/config", configPayment);
