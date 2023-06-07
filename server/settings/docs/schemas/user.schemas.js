b891b0f1077ba00b297a50832531fd50843310b8
JOEL18VEGAS <joel18gonzalez@outlook.com>
2023-06-06 21:52:08 -0500

    fix/user(backend/user):User routes Modified and Documented

--- server/settings/docs/schemas/user.schemas.js
+++ server/settings/docs/schemas/user.schemas.js
@@ -9,75 +9,174 @@
  *         - last_name
  *         - user_name
  *         - email
+ *         - password
  *         - date_birth
  *       properties:
  *         first_name:
  *           type: string
  *           maxLength: 50
  *           minLength: 1
- *           description: Nombre del usuario.
+ *           description: User's first name.
  *         last_name:
  *           type: string
  *           maxLength: 50
  *           minLength: 1
- *           description: Apellido del usuario.
+ *           description: User's last name.
  *         user_name:
  *           type: string
  *           maxLength: 50
  *           minLength: 1
- *           description: Nombre de usuario del usuario.
+ *           description: User's username.
  *         profile_picture:
  *           type: string
  *           maxLength: 50
  *           minLength: 1
- *           description: URL de la imagen de perfil del usuario.
+ *           description: URL of the user's profile picture.
  *         email:
  *           type: string
  *           format: email
  *           maxLength: 50
  *           minLength: 1
- *           description: Dirección de correo electrónico del usuario.
+ *           description: User's email address.
+ *         password:
+ *           type: string
+ *           maxLength: 50
+ *           minLength: 8
+ *           description: User's password.
  *         date_birth:
  *           type: string
  *           format: date
- *           description: Fecha de nacimiento del usuario.
+ *           description: User's date of birth.
  *         is_tutor:
  *           type: boolean
- *           description: Indica si el usuario es un tutor o no.
+ *           description: Indicates whether the user is a tutor or not.
  *         about_me:
  *           type: string
- *           description: Información sobre el usuario.
+ *           description: Information about the user.
  *       example:
- *         first_name: John
- *         last_name: Doe
- *         user_name: johndoe
- *         profile_picture: "https://example.com/image.jpg"
- *         email: johndoe@example.com
+ *         first_name: joel
+ *         last_name: vegas
+ *         user_name: joelvegas2023
+ *         email: joelvegas@gmail.com
+ *         password: password123
  *         date_birth: 1990-01-01
- *         is_tutor: false
- *         about_me: Soy desarrollador de software.
  * 
  *     UserResponse:
  *       type: object
  *       properties:
- *         201:
- *           type: boolean
- *           description: Indica si la solicitud se procesó correctamente.
+ *         status:
+ *           type: number
+ *           example: 201
+ *         message:
+ *           type: string
+ *           example: User created successfully
  *         data:
  *           $ref: '#/components/schemas/User'
  *       example:
- *         id: "90cf919a-fb0c-4101-968f-d097fdc43b82"
- *         profile_picture: "https://example.com/image.jpg"
- *         is_staff: false
- *         is_active: true
- *         first_name: "John"
- *         last_name: "Doe"
- *         user_name: "johndoe"
- *         email: "johndoe@example.com"
- *         date_birth: "1990-01-01"
- *         is_tutor: false
- *         about_me: "Soy desarrollador de software."
- *         updatedAt: "2021-08-31T04:00:00.000Z"
- *         createdAt: "2021-08-31T04:00:00.000Z"
+ *         status: 201
+ *         message: User created successfully
+ *         data:
+ *           id: "90cf919a-fb0c-4101-968f-d097fdc43b82"
+ *           profile_picture: "https://example.com/image.jpg"
+ *           is_staff: false
+ *           is_active: true
+ *           first_name: "joel"
+ *           last_name: "vegas"
+ *           user_name: "joelvegas2023"
+ *           email: "joelvegas@gmail.com"
+ *           date_birth: "1990-01-01"
+ *           is_tutor: false
+ *           about_me: "I am a software developer."
+ *           updatedAt: "2021-08-31T04:00:00.000Z"
+ *           createdAt: "2021-08-31T04:00:00.000Z"
  * 
+ */
+
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     LoginRequest:
+ *       type: object
+ *       required:
+ *         - email
+ *         - password
+ *       properties:
+ *         email:
+ *           type: string
+ *           format: email
+ *           description: User's email address.
+ *         password:
+ *           type: string
+ *           maxLength: 50
+ *           minLength: 8
+ *           description: User's password.
+ * 
+ *     LoginResponse:
+ *       type: object
+ *       properties:
+ *         status:
+ *           type: number
+ *           example: 200
+ *         message:
+ *           type: string
+ *           example: User logged in successfully
+ *         data:
+ *           type: object
+ *           properties:
+ *             id:
+ *               type: string
+ *               example: "90cf919a-fb0c-4101-968f-d097fdc43b82"
+ *             first_name:
+ *               type: string
+ *               example: "joel"
+ *             last_name:
+ *               type: string
+ *               example: "vegas"
+ *             user_name:
+ *               type: string
+ *               example: "joelvegas2023"
+ *             email:
+ *               type: string
+ *               example: "joelvegas@gmail.com"
+ *             date_birth:
+ *               type: string
+ *               example: "1990-01-01"
+ *             token:
+ *               type: string
+ *               example: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Im5vbWJyZSIsImlhdCI6MTYzMDIyNjg3OSwiZXhwIjoxNjMwMjI3Njc5fQ.HkiWqBAMHLG2D6S...Xzdaomq4RUV0lvN-XvdDl9w"
+ *             profile_picture:
+ *               type: string
+ *               example: "https://example.com/image.jpg"
+ *       example:
+ *         status: 200
+ *         message: User logged in successfully
+ *         data:
+ *           id: "90cf919a-fb0c-4101-968f-d097fdc43b82"
+ *           first_name: "joel"
+ *           last_name: "vegas"
+ *           user_name: "joelvegas2023"
+ *           email: "joelvegas@gmail.com"
+ *           date_birth: "1990-01-01"
+ *           token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Im5vbWJyZSIsImlhdCI6MTYzMDIyNjg3OSwiZXhwIjoxNjMwMjI3Njc5fQ.HkiWqBAMHLG2D6S...Xzdaomq4RUV0lvN-XvdDl9w"
+ *           profile_picture: "https://example.com/image.jpg"
+ */
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     ConfirmAccountResponse:
+ *       type: object
+ *       properties:
+ *         status:
+ *           type: number
+ *           example: 200
+ *         message:
+ *           type: string
+ *           example: Your account has been confirmed successfully
+ *       example:
+ *         status: 200
+ *         message: Your account has been confirmed successfully
  */
